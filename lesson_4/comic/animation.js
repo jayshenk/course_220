9c90695d03b27b9ed29434f101088034a534a87f
Jay Shenk <jay.shenk@gmail.com>
2016-10-26 19:31:34 -0400

    Add Start Over link and refactor

--- lesson_4/comic/animation.js
+++ lesson_4/comic/animation.js
@@ -4,14 +4,24 @@ $(function() {
   var speed = 250;
   var delay = 1500;
 
-  $blinds.each(function(i) {
-    var $blind = $blinds.eq(i);
-    var totalDelay = delay * i + speed;
+  function startAnimation() {
+    $blinds.each(function(i) {
+      var $blind = $blinds.eq(i);
+      var totalDelay = delay * i + speed;
 
-    $blind.delay(totalDelay).animate({
-      top: $blind.position().top + $blind.height(),
-      height: 0,
-    }, speed);
+      $blind.delay(totalDelay).animate({
+        top: "+=" + $blind.height(),
+        height: 0,
+      }, speed);
+    });
+  }
+
+  $("a").on("click", function(e) {
+    e.preventDefault();
+
+    $blinds.removeAttr("style");
+    startAnimation();
   });
 
+  startAnimation();
 });

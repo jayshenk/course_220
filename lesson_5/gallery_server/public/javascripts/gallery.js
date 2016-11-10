008026d95336b5e5c5413bcd47b8b798d82ab6e2
Jay Shenk <jay.shenk@gmail.com>
2016-11-10 14:01:21 -0500

    Initial solution for Step 2: Render Comments for the First Photo

--- lesson_5/gallery_server/public/javascripts/gallery.js
+++ lesson_5/gallery_server/public/javascripts/gallery.js
@@ -1,11 +1,14 @@
 $(function() {
   var templates = {};
   var photos;
+  var comments;
 
   $("script[type='text/x-handlebars']").each(function() {
     var $tmpl = $(this);
     templates[$tmpl.attr("id")] = Handlebars.compile($tmpl.html());
   });
+
+  Handlebars.registerPartial("comment", $("#comment").html());
 
   $.ajax({
     url: "/photos",
@@ -13,6 +16,14 @@ $(function() {
       photos = json;
       $("#slides").html(templates.photos({ photos: photos }));
       $("section > header").html(templates.photo_information(photos[0]));
+    }
+  });
+
+  $.ajax({
+    url: "/comments?photo_id=1",
+    success: function(json) {
+      comments = json;
+      $("#comments > ul").html(templates.comments({ comments: comments }));
     }
   });
 });

15d700fce3595f322ed90363620be1ceb71d5f27
Jay Shenk <jay.shenk@gmail.com>
2016-11-10 14:19:40 -0500

    Refactor Step 2 with tutorial

--- lesson_5/gallery_server/public/javascripts/gallery.js
+++ lesson_5/gallery_server/public/javascripts/gallery.js
@@ -1,29 +1,42 @@
 $(function() {
   var templates = {};
   var photos;
-  var comments;
 
   $("script[type='text/x-handlebars']").each(function() {
     var $tmpl = $(this);
     templates[$tmpl.attr("id")] = Handlebars.compile($tmpl.html());
   });
 
-  Handlebars.registerPartial("comment", $("#comment").html());
+  $("[data-type=partial]").each(function() {
+    var $partial = $(this);
+    Handlebars.registerPartial($partial.attr("id"), $partial.html());
+  });
 
   $.ajax({
     url: "/photos",
     success: function(json) {
       photos = json;
-      $("#slides").html(templates.photos({ photos: photos }));
-      $("section > header").html(templates.photo_information(photos[0]));
+      renderPhotos();
+      renderPhotoInformation(0);
+      getCommentsFor(photos[0].id);
     }
   });
 
-  $.ajax({
-    url: "/comments?photo_id=1",
-    success: function(json) {
-      comments = json;
-      $("#comments > ul").html(templates.comments({ comments: comments }));
-    }
-  });
+  function renderPhotos() {
+    $("#slides").html(templates.photos({ photos: photos }));
+  }
+
+  function renderPhotoInformation(idx) {
+    $("section > header").html(templates.photo_information(photos[idx]));    
+  }
+
+  function getCommentsFor(idx) {
+    $.ajax({
+      url: "/comments",
+      data: "photo_id=" + idx,
+      success: function(comment_json) {
+        $("#comments ul").html(templates.comments({ comments: comment_json }));
+      }
+    });
+  }
 });

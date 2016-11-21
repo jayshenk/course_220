6c1c1ad5caffbbf69fe7b5ef7def5c1b924cc32c
Jay Shenk <jay.shenk@gmail.com>
2016-11-21 17:26:34 -0500

    Complete Exercise 1

---
+++ lesson_7/constructor_functions_and_prototypes_1/exercise_1.js
@@ -0,0 +1,13 @@
+function Circle(radius) {
+  this.radius = radius;
+}
+
+Circle.prototype.area = function() {
+  return Math.PI * Math.pow(this.radius, 2);
+}
+
+var a = new Circle(3);
+var b = new Circle(4);
+
+console.log(a.area().toFixed(2)); // 28.27
+console.log(b.area().toFixed(2)); // 50.27

54e21a744d6b388ca2b5037d9912b8d9662442ea
Jay Shenk <jay.shenk@gmail.com>
2016-11-27 10:41:16 -0500

    Complete Guess a Word, Part 2: Core Functions

--- lesson_7/guess_word/javascripts/game.js
+++ lesson_7/guess_word/javascripts/game.js
@@ -1,9 +1,43 @@
+var $message = $('#message');
+var $letters = $('#spaces');
+var $guesses = $('#guesses');
+var $apples = $('#apples');
+
 var randomWord = function() {
   var words = ['apple', 'banana', 'orange', 'pear'];
+  var idx = Math.floor(Math.random() * words.length);
 
-  return function() {
-    var idx = Math.floor(Math.random() * words.length);
+  return words.splice(idx, 1)[0];  
+}
 
-    return words.splice(idx, 1)[0];
-  };
-}();
+var Game = {
+  word: '',
+  incorrect: 0,
+  lettersGuessed: [],
+  maxWrongGuesses: 6,
+
+  createBlanks: function() {
+    var spaces = (new Array(this.word.length + 1)).join('<span></span>');
+
+    $letters.find('span').remove();
+    $letters.append(spaces);
+    this.$spaces = $('#spaces span');
+  },
+
+  displayMessage: function(text) {
+    $message.text(text);
+  },
+
+  init: function() {
+    this.word = randomWord();
+    if (!this.word) {
+      this.displayMessage('Sorry, I\'ve run out of words!');
+    }
+    this.word = this.word.split('');
+    this.createBlanks();
+    return this;
+  }
+}
+
+var game = Object.create(Game);
+game.init();
